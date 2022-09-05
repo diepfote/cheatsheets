ed2a89026aff4c504289acc6ba7cc1f0c13ff8cc
Florian Begusch <florian.begusch@gmail.com>
2022-09-05 06:09:17 +0000

    2022-09-05T06:09+00:00

--- darwin/homebrew.sh
+++ darwin/homebrew.sh
@@ -1,7 +1,18 @@
 # Homebrew
 
 ## install previous version of pkg
-brew tap-new homebrew/<formula-name>
-brew extract <formula-name> homebrew/<formula-name> --version <version>
-brew install <formula-name>@<version>
 
+```
+cd "$(brew --repo homebrew/core)"
+
+# find commit & reset to commit
+git find-commits-changed-file -- Formula/poetry.rb
+git reset --hard 1b9787d7c5
+
+# build from source
+brew install --build-from-source poetry
+
+# reset homebrew repo
+git fetch origin master
+git reset --hard origin/master
+```
