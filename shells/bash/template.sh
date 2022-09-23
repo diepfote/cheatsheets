99c9c5354e7228a8ba2b03ae4c665c83ce15e459
Florian Begusch <florian.begusch@gmail.com>
2022-09-23 04:22:28 +0000

    2022-09-23T04:22+00:00

--- shells/bash/template.sh
+++ shells/bash/template.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# all of these stem from https://www.shellcheck.net/wiki/
 set -o pipefail  # propagate errors
 set -u  # exit on undefined
 set -e  # exit on non-zero return value
 #set -f  # disable globbing/filename expansion
 shopt -s failglob  # error on unexpaned globs
+shopt -s inherit_errexit  # Bash disables set -e in command substitution by default; reverse this behavior
 
 
 _help() {
